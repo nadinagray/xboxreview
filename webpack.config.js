c173cc2ad09162b4780019ab5b200997cfd71895
Nadina <nadina@soulmates.ai>
2020-06-17 10:57:59 -0700

    Other webpack tweaks

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack') // compiles js modules
+const path = require('path')
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+module.exports = {
+  entry: ['./src/js/index.js'],
+  output: {},
+  module: {},
+  plugins: [],
+  devServer: {},
+  devtool: {},
+  optimization: {}
+}
